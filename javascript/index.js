f484a7b6381cec6b05058a0ea00d6a176d342359
arisa.kato <arisa.kato@carrot.cojp>
2021-11-17 10:31:22 +0900

    セレクトボックス選択状態のjs追加

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 // デザイン切り替え
 function chenge_css_btn() {
   var css = "";
+  var select_box = document.getElementById("select_design");
   var elem = document.getElementById("css_design");
   var select_design = document.getElementById("select_design").value;
   switch (select_design) {
@@ -21,18 +22,24 @@ function chenge_css_btn() {
       alert('変更できませんでした。');
       break;
   }
+  var select_box = document.getElementById("select_design");
   var home_css = "../stylesheets/" + css;
   elem.href = home_css;
   localStorage.setItem('css_design_name', css);
+  localStorage.setItem('css_design_index', index_num);
 }
 
 // デザイン読み込み
 window.onload = function(){
   var elem = document.getElementById("css_design");
-
   var load_design = localStorage.getItem('css_design_name');
+  var load_design_index = localStorage.getItem('css_design_index');
+
   if (load_design != null) {
     var home_css = "../stylesheets/" + load_design;
     elem.href = home_css;
+    var select_box = document.getElementById("select_design");
+    select_box.options[load_design_index].selected = true;
+    console.log(load_design_index);
   }
 }
--- javascript/page.js
+++ javascript/page.js
@@ -1,6 +1,7 @@
 // デザイン切り替え
 function chenge_css_btn() {
   var css = "";
+  var select_box = document.getElementById("select_design");
   var elem = document.getElementById("css_design");
   var select_design = document.getElementById("select_design").value;
   switch (select_design) {
@@ -21,18 +22,23 @@ function chenge_css_btn() {
       alert('変更できませんでした。');
       break;
   }
+  var index_num = select_box.selectedIndex;
   var page_css = "../../stylesheets/" + css;
   elem.href = page_css;
   localStorage.setItem('css_design_name', css);
+  localStorage.setItem('css_design_index', index_num);
 }
 
 // デザイン読み込み
 window.onload = function(){
   var elem = document.getElementById("css_design");
-
   var load_design = localStorage.getItem('css_design_name');
+  var load_design_index = localStorage.getItem('css_design_index');
+
   if (load_design != null) {
     var page_css = "../../stylesheets/" + load_design;
     elem.href = page_css;
+    var select_box = document.getElementById("select_design");
+    select_box.options[load_design_index].selected = true;
   }
 }
